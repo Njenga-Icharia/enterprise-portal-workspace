ba91b00b15df4fb6c5d3a7c265af6c6ae11fa68f
Njenga Icharia <icharianjenga@gmail.com>
2026-09-15 15:34:16 +0300

    feat(engineering): add scroll curtain engineering variant

---
+++ src/components/ScrollCurtainEngineering.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScrollCurtain, { ScrollCurtainItem } from './ScrollCurtain';
+
+const ENGINEERING_IMAGES: ScrollCurtainItem[] = [
+  { id: 'img1', imageUrl: 'https://msftstories.thesourcemediaassets.com/sites/133/2018/06/MTC-2-960x640.jpg' },
+  { id: 'img2', imageUrl: 'https://msftstories.thesourcemediaassets.com/sites/133/2018/06/MTC-1.jpg' },
+  { id: 'img3', imageUrl: 'https://msftstories.thesourcemediaassets.com/sites/133/2018/06/MTC-3-1024x683.jpg' },
+];
+
+export default function ScrollCurtainEngineering() {
+  return <ScrollCurtain items={ENGINEERING_IMAGES} />;
+}
