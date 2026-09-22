b6d81dbee8c82d0720ae198399e16c927f136c11
Njenga Icharia <icharianjenga@gmail.com>
2026-09-22 14:48:12 +0300

    feat: add BPO page, FlipWindow component, and logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,26 @@ import dynamic from "next/dynamic";
 import Link from "next/link";
 
 import ContactForm from "@/components/ContactForm";
-import HeroCloud from "@/components/LocalComponentsCloud/HeroCloud";
-import BlurryCarouselCloud from "@/components/LocalComponentsCloud/BlurryCarouselCloud";
-import ResponsiveWindowCloud from "@/components/LocalComponentsCloud/ResponsiveWindowCloud";
-import CTACloud from "@/components/LocalComponentsCloud/CTACloud";
-import AboutCloud from "@/components/LocalComponentsCloud/AboutCloud";
+// import HeroCloud from "@/components/LocalComponentsCloud/HeroCloud";
+// import BlurryCarouselCloud from "@/components/LocalComponentsCloud/BlurryCarouselCloud";
+// import ResponsiveWindowCloud from "@/components/LocalComponentsCloud/ResponsiveWindowCloud";
+// import CTACloud from "@/components/LocalComponentsCloud/CTACloud";
+// import AboutCloud from "@/components/LocalComponentsCloud/AboutCloud";
+import FlipWindowBpo from "@/components/LocalComponentsCloud/FlipWindowBpo";
 
 export default function CloudPage() {
   return (
     <div className="relative min-h-screen bg-[#f8f9fa] text-[#1e1e28] overflow-x-clip font-sans">
       <div className="w-full relative">
-        <HeroCloud />
-        <ResponsiveWindowCloud />
+        {/* <HeroCloud />
+        <ResponsiveWindowCloud /> */}
       </div>
 
       <div className="relative overflow-x-hidden">
-        <AboutCloud />
+        <FlipWindowBpo />
+        {/* <AboutCloud />
         <BlurryCarouselCloud />
-        <CTACloud />
+        <CTACloud /> */}
         <ContactForm />
       </div>
       
