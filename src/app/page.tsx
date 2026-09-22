50ecf207802663d4efbfce9a5004a88e4a9d9fe4
Njenga Icharia <icharianjenga@gmail.com>
2026-09-22 17:44:29 +0300

    refactor: swap BPO page hero to SpotlightGridBpo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 
 import ContactForm from "@/components/ContactForm";
 import FlipWindowBpo from "@/components/LocalComponentsBpo/FlipWindowBpo";
+import SpotlightGridBpo from "@/components/LocalComponentsBpo/SpotlightGridBpo";
+// import SpotlightGridBpo1 from "@/components/LocalComponentsBpo/SpotlightGridBpo1";
 
 export default function CloudPage() {
   return (
       <div className="relative overflow-x-hidden">
+        <SpotlightGridBpo />
+        {/* <SpotlightGridBpo1 /> */}
         <FlipWindowBpo />
         <ContactForm />
       </div>
